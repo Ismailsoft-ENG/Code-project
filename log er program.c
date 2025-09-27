038f0b973ebc658e874ed45442209cbaf89276f7
Ismailsoft-ENG <ismail1234046@gmail.com>
2025-09-27 09:49:58 +0600

    Add files via upload

---
+++ log er program.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main ()
+{
+     double x=120;
+     double result;
+
+     result = log(x);
+     printf(" the log is :%.3lf",result);
+
+    return 0;
+   }
